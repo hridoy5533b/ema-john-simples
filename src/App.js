c2aca3bc15aead790ca2e20cba0aaa58914787e3
hridoyahmed <hridoythechotobhai@gmail.com>
2020-10-29 02:30:10 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+import Product from './components/Product/Product';
+import Shops from './components/Shops/Shops';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Shops></Shops>
+    </div>
+  );
+}
+
+export default App;
